9773e290d7f31ffabf09013983eb61e99300ca14
Ozair <93428647+Ozair-khaan@users.noreply.github.com>
2023-06-12 12:59:04 +0530

    first commit

---
+++ Spring-Validation/src/main/java/com/practice/Controller/MyController.java
@@ -0,0 +1,28 @@
+package com.practice.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.practice.entities.LoginData;
+
+@Controller
+public class MyController {
+
+	@GetMapping("/form")
+	public String openForm(Model model) {
+		System.out.println("Opening form");
+		model.addAttribute("loginData", new LoginData());
+		return "form";
+
+	}
+	
+	//handler for process form
+	@PostMapping("/process")
+	public String processForm(@ModelAttribute("loginData") LoginData loginData) {
+		return "success";
+	}
+
+}
