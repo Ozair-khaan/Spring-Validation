ba7dc4d41c3d104056386d05a9ae5b84b10221ee
Ozair Khan <ozair.khan@viraltech.in>
2023-06-13 21:16:06 +0530

    try

--- Spring-Validation/src/main/java/com/practice/Controller/MyController.java
+++ Spring-Validation/src/main/java/com/practice/Controller/MyController.java
@@ -2,11 +2,14 @@ package com.practice.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.practice.entities.LoginData;
+
+import javax.validation.Valid;
 
 @Controller
 public class MyController {
@@ -21,7 +24,14 @@ public class MyController {
 	
 	//handler for process form
 	@PostMapping("/process")
-	public String processForm(@ModelAttribute("loginData") LoginData loginData) {
+	public String processForm(@Valid @ModelAttribute("loginData") LoginData loginData, BindingResult result) {
+
+		if(result.hasErrors()){
+			System.out.println(result);
+			return "form";
+		}
+
+		System.out.println(loginData);
 		return "success";
 	}
 
